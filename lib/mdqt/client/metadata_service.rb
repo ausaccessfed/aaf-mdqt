c6cbda4a8e24a3f7f937a2f046fa49f6321227b4
Jack <5182053+phyzical@users.noreply.github.com>
2025-03-06 14:52:12 +0800

    Feature/add content type header (#35)
    * add content-type -header
    
    * version bump

--- lib/mdqt/client/metadata_service.rb
+++ lib/mdqt/client/metadata_service.rb
@@ -156,6 +156,7 @@ module MDQT
           end
           faraday.ssl.verify = tls_cert_check?
           faraday.headers['Accept'] = 'application/samlmetadata+xml'
+          faraday.headers['Content-Type'] = 'application/samlmetadata+xml'
           faraday.headers['Accept-Charset'] = 'utf-8'
           faraday.headers['User-Agent'] = "MDQT v#{MDQT::VERSION}"
           #faraday.adapter :httpx
--- lib/mdqt/version.rb
+++ lib/mdqt/version.rb
@@ -1,3 +1,3 @@
 module MDQT
-  VERSION = "0.8.15"
+  VERSION = "0.8.16"
 end
